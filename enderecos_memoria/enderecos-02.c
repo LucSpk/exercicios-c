d7741d69e757d530d62ba3c1187185cca18b6061
Lucas Alves <olalvezluc@gmail.com>
2026-02-22 17:01:36 -0300

    Adiciona exemplos de endereços

--- enderecos_memoria/enderecos-02.c
+++ enderecos_memoria/enderecos-02.c
@@ -11,16 +11,20 @@ int main() {
     printf("Tanho de um char: %d\n", sizeof(char));
     printf("Tanho de um int: %d\n", sizeof(int));
     
-    printf("c: (%p)\n", &c);
-    printf("c + 1: (%p)\n", &c + 1);
-    printf("i: (%p)\n", &i);
-    printf("str: (%p)\n", &str);
-    printf("str.x: (%p)\n", &str.x);
-    printf("str.y: (%p)\n", &str.y);
-    printf("arr[0]: (%p)\n", &arr[0]);
-    printf("arr[1]: (%p)\n", &arr[1]);
-    printf("arr[2]: (%p)\n", &arr[2]);
-    printf("arr[2]: (%p)\n", &arr[3]);
+    printf("&c: (%p)\n", &c);
+    printf("&c + 1: (%p)\n", &c + 1);
+
+    printf("&i: (%p)\n", &i);
+    printf ("(void *) &i = (%p)\n", (void *) &i);
+    printf ("(long int) &i = (%ld)\n", (long int) &i); 
+
+    printf("&str: (%p)\n", &str);
+    printf("&str.x: (%p)\n", &str.x);
+    printf("&str.y: (%p)\n", &str.y);
+    printf("&arr[0]: (%p)\n", &arr[0]);
+    printf("&arr[1]: (%p)\n", &arr[1]);
+    printf("&arr[2]: (%p)\n", &arr[2]);
+    printf("&arr[2]: (%p)\n", &arr[3]);
 
     return 0;
 }
