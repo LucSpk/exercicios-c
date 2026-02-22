f169627b46df5f6224fd73d98f864ba94bc200fd
Lucas Alves <olalvezluc@gmail.com>
2026-02-22 16:52:45 -0300

    exercicio 2 de enderecos

---
+++ enderecos_memoria/enderecos-02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    int i;
+    struct {
+        int x, y;
+    } str;
+    int arr[4];
+
+    printf("Tanho de um char: %d\n", sizeof(char));
+    printf("Tanho de um int: %d\n", sizeof(int));
+    
+    printf("c: (%p)\n", &c);
+    printf("i: (%p)\n", &i);
+    printf("str: (%p)\n", &str);
+    printf("str.x: (%p)\n", &str.x);
+    printf("str.y: (%p)\n", &str.y);
+    printf("arr[0]: (%p)\n", &arr[0]);
+    printf("arr[1]: (%p)\n", &arr[1]);
+    printf("arr[2]: (%p)\n", &arr[2]);
+    printf("arr[2]: (%p)\n", &arr[3]);
+
+    return 0;
+}
